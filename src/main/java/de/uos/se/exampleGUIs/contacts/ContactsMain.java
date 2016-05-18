6691822626c3e1493fdd90c78878cfb58b2647dc
jps <jschleutker@uos.de>
2016-05-18 11:38:00 +0200

    added new gui which is able to maintain contacts. however some exceptions arise ;)

--- src/main/java/de/uos/se/exampleGUIs/contacts/ContactsMain.java
+++ src/main/java/de/uos/se/exampleGUIs/contacts/ContactsMain.java
@@ -5,11 +5,9 @@ import de.uos.se.exampleGUIs.contacts.view.Contacts;
 import java.awt.*;
 
 /**
- * Created by sem on 08.04.2016.
- * <p>
  * Contains the main-class, the entry point.
  */
-public class Main
+public class ContactsMain
 {
     public static void main(String[] args)
     {
